b5534967a3e86e81a9a26696b238343de813025e
danmera1980 <danmera1980@gmail.com>
2022-06-19 14:08:12 -0500

    Calculator JS complete

--- scripts/calculator.js
+++ scripts/calculator.js
@@ -6,7 +6,16 @@ let calculate = document.querySelector('#calculate');
 
 calculate.addEventListener('click', function (e) {
     e.preventDefault();
-    let result = salary.value * years.value * days.value;
-    bonus.innerHTML = "$"+result;
-    console.log(result);
+    let result;
+    let daysSalary = parseInt(salary.value)/30
+    if(years.value < 1){
+        result = (parseInt(days.value) * daysSalary * 15)/365;
+    } else if(years.value < 3){
+        result = (daysSalary * 15);
+    } else if(years.value < 10){
+        result = (daysSalary * 19);
+    } else {
+        result = (daysSalary * 21);
+    }
+    bonus.innerHTML = '$' + result.toFixed(2);
 });
